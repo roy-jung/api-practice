8b6c0f82bea0dc0ad56bc929281d06176dcfcd8f
roy-jung <power4ce@gmail.com>
2021-07-19 19:38:08 +0900

    apollo server update

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,8 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import cors from 'cors'
 import resolvers from './resolvers/index.js'
 import schema from './schema/index.js'
 import { readDB } from './dbController.js'
-
-const app = express()
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }),
-)
 
 const server = new ApolloServer({
   typeDefs: schema,
@@ -24,8 +15,16 @@ const server = new ApolloServer({
   },
 })
 
-server.applyMiddleware({ app, path: '/graphql' })
-
-app.listen(8000, () => {
-  console.log('server listening on 8000...')
+const app = express()
+await server.start()
+server.applyMiddleware({
+  app,
+  path: '/graphql',
+  cors: {
+    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+    credentials: true,
+  },
 })
+
+await app.listen({ port: 8000 })
+console.log('server listening on 8000...')
